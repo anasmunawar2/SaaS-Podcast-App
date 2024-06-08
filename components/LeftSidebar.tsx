39fdfad4266799686a4ec5580aeaa0cb715d9523
anasmunawar2 <anasmunawar2@gmail.com>
2024-06-08 19:49:37 +0500

    leftsidebar

---
+++ components/LeftSidebar.tsx
@@ -0,0 +1,31 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const LeftSidebar = () => {
+  return (
+    <section className="left_sidebar">
+      <nav className="flex flex-col gap-6">
+        <Link
+          href="/"
+          className="flex cursor-pointer items-center gap-1 pb-10 max-lg:justify-center"
+        >
+          <Image src="/icons/logo.svg" alt="logo" width={23} height={27} />
+          <h1 className="text-24 font-extrabold text-white max-lg:hidden">
+            Podcast
+          </h1>
+        </Link>
+
+        {[
+          {
+            route: "/profile",
+            label: "Profile",
+            imgUrl: "/icons/microphone.svg",
+          },
+        ]}
+      </nav>
+    </section>
+  );
+};
+
+export default LeftSidebar;
