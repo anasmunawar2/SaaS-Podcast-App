0f19fa73dd8f42aa3e31900d505544c09982faf6
anasmunawar2 <anasmunawar2@gmail.com>
2024-06-13 03:41:44 +0500

    clerk-integrated

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+  providers: [
+    {
+      domain: "https://outgoing-yeti-27.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
+
+export default authConfig;
